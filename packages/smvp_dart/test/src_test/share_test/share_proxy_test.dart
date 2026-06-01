5dd71e574f446835ee38c7b12145848db12c7e42
sl8s <sl8sqw@gmail.com>
2026-06-01 22:17:44 +0300

    refactoring(packages/smvp_dart/test/src_test/share_test): formatted and all tests have the name of one method, not 2 or more

--- packages/smvp_dart/test/src_test/share_test/share_proxy_test.dart
+++ packages/smvp_dart/test/src_test/share_test/share_proxy_test.dart
@@ -10,143 +10,127 @@ final class MockCallback extends Mock implements Callback {}
 
 void main() {
   group(
-      "ShareProxy",
-      () => {
-            test("getValue<T>(String key, T defaultValue)", () {
-              final shareProxy = ShareProxy();
-              expect(shareProxy.getValue<String>("key", "qwerty"),
-                  equals("qwerty"));
-              shareProxy.update("key", "ytrewq");
-              expect(shareProxy.getValue<String>("key", "qwerty"),
-                  equals("ytrewq"));
-            }),
-            test("update(String key, dynamic value)", () {
-              final shareProxy = ShareProxy();
-              shareProxy.update("key", "ytrewq");
-              expect(shareProxy.getValue<String>("key", "qwerty"),
-                  equals("ytrewq"));
-            }),
-            test("delete(String key)", () {
-              final shareProxy = ShareProxy();
-              shareProxy.update("key", "ytrewq");
-              shareProxy.delete("key");
-              expect(shareProxy.getValue<String>("key", "qwerty"),
-                  equals("qwerty"));
-            }),
-            test(
-                "addListener(String key, void Function(dynamic event) callback), "
-                "notifyListener(String key, dynamic value)", () {
-              final mockCallback = MockCallback();
-              final shareProxy = ShareProxy();
-              shareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.notifyListener("key", "hello");
-              verify(() => mockCallback.onCallback("hello")).called(1);
-            }),
-            test(
-                "addListener(String key, void Function(dynamic event) callback), "
-                "notifyListeners(String key, dynamic value)", () {
-              final mockCallback = MockCallback();
-              final shareProxy = ShareProxy();
-              final secondShareProxy = ShareProxy();
-              shareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.notifyListeners("key", "hello");
-              verify(() => mockCallback.onCallback("hello")).called(2);
-            }),
-            test(
-                "addListener(String key, void Function(dynamic event) callback), "
-                "notifyListeners(String key, dynamic value), "
-                "deleteAllListenersByKey(String key)", () {
-              final mockCallback = MockCallback();
-              final shareProxy = ShareProxy();
-              final secondShareProxy = ShareProxy();
-              shareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.deleteAllListenersByKey("key");
-              shareProxy.notifyListeners("key", "hello");
-              verifyNever(() => mockCallback.onCallback("hello"));
-            }),
-            test(
-                "addListener(String key, void Function(dynamic event) callback), "
-                "notifyListeners(String key, dynamic value), "
-                "deleteAllListenersByListKey(List<String> listKey)", () {
-              final mockCallback = MockCallback();
-              final shareProxy = ShareProxy();
-              final secondShareProxy = ShareProxy();
-              shareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.addListener("keyTwo", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("keyTwo", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.deleteAllListenersByListKey(["key", "keyTwo"]);
-              shareProxy.notifyListeners("key", "hello");
-              shareProxy.notifyListeners("keyTwo", "hello");
-              verifyNever(() => mockCallback.onCallback("hello"));
-            }),
-            test(
-                "addListener(String key, void Function(dynamic event) callback), "
-                "notifyListeners(String key, dynamic value), "
-                "deleteListenerByListenerId(String key)", () {
-              final mockCallback = MockCallback();
-              final shareProxy = ShareProxy();
-              final secondShareProxy = ShareProxy();
-              shareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.addListener("keyTwo", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("keyTwo", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.deleteListenerByListenerId("key");
-              shareProxy.notifyListeners("key", "hello");
-              shareProxy.notifyListeners("keyTwo", "hello");
-              verify(() => mockCallback.onCallback("hello")).called(3);
-            }),
-            test(
-                "addListener(String key, void Function(dynamic event) callback), "
-                "notifyListeners(String key, dynamic value), "
-                "deleteListenersByListenerId(List<String> listKey)", () {
-              final mockCallback = MockCallback();
-              final shareProxy = ShareProxy();
-              final secondShareProxy = ShareProxy();
-              shareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("key", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.addListener("keyTwo", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              secondShareProxy.addListener("keyTwo", (event) {
-                mockCallback.onCallback(event as String);
-              });
-              shareProxy.deleteListenersByListenerId(["key", "keyTwo"]);
-              shareProxy.notifyListeners("key", "hello");
-              shareProxy.notifyListeners("keyTwo", "hello");
-              verify(() => mockCallback.onCallback("hello")).called(2);
-            }),
+    "ShareProxy",
+    () {
+      test("getValue<T>(String key, T defaultValue)", () {
+        final shareProxy = ShareProxy();
+        expect(shareProxy.getValue<String>("key", "qwerty"), equals("qwerty"));
+        shareProxy.update("key", "ytrewq");
+        expect(shareProxy.getValue<String>("key", "qwerty"), equals("ytrewq"));
+      });
+      test("update(String key, dynamic value)", () {
+        final shareProxy = ShareProxy();
+        shareProxy.update("key", "ytrewq");
+        expect(shareProxy.getValue<String>("key", "qwerty"), equals("ytrewq"));
+      });
+      test("delete(String key)", () {
+        final shareProxy = ShareProxy();
+        shareProxy.update("key", "ytrewq");
+        shareProxy.delete("key");
+        expect(shareProxy.getValue<String>("key", "qwerty"), equals("qwerty"));
+      });
+      test(
+        "addListener(String key, void Function(dynamic event) callback)",
+        () {
+          final mockCallback = MockCallback();
+          final shareProxy = ShareProxy();
+          shareProxy.addListener("key", (event) {
+            mockCallback.onCallback(event as String);
           });
+          shareProxy.notifyListener("key", "hello");
+          verify(() => mockCallback.onCallback("hello")).called(1);
+        },
+      );
+      test("notifyListeners(String key, dynamic value)", () {
+        final mockCallback = MockCallback();
+        final shareProxy = ShareProxy();
+        final secondShareProxy = ShareProxy();
+        shareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.notifyListeners("key", "hello");
+        verify(() => mockCallback.onCallback("hello")).called(2);
+      });
+      test("deleteAllListenersByKey(String key)", () {
+        final mockCallback = MockCallback();
+        final shareProxy = ShareProxy();
+        final secondShareProxy = ShareProxy();
+        shareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.deleteAllListenersByKey("key");
+        shareProxy.notifyListeners("key", "hello");
+        verifyNever(() => mockCallback.onCallback("hello"));
+      });
+      test("deleteAllListenersByListKey(List<String> listKey)", () {
+        final mockCallback = MockCallback();
+        final shareProxy = ShareProxy();
+        final secondShareProxy = ShareProxy();
+        shareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.addListener("keyTwo", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("keyTwo", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.deleteAllListenersByListKey(["key", "keyTwo"]);
+        shareProxy.notifyListeners("key", "hello");
+        shareProxy.notifyListeners("keyTwo", "hello");
+        verifyNever(() => mockCallback.onCallback("hello"));
+      });
+      test("deleteListenerByListenerId(String key)", () {
+        final mockCallback = MockCallback();
+        final shareProxy = ShareProxy();
+        final secondShareProxy = ShareProxy();
+        shareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.addListener("keyTwo", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("keyTwo", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.deleteListenerByListenerId("key");
+        shareProxy.notifyListeners("key", "hello");
+        shareProxy.notifyListeners("keyTwo", "hello");
+        verify(() => mockCallback.onCallback("hello")).called(3);
+      });
+      test("deleteListenersByListenerId(List<String> listKey)", () {
+        final mockCallback = MockCallback();
+        final shareProxy = ShareProxy();
+        final secondShareProxy = ShareProxy();
+        shareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("key", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.addListener("keyTwo", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        secondShareProxy.addListener("keyTwo", (event) {
+          mockCallback.onCallback(event as String);
+        });
+        shareProxy.deleteListenersByListenerId(["key", "keyTwo"]);
+        shareProxy.notifyListeners("key", "hello");
+        shareProxy.notifyListeners("keyTwo", "hello");
+        verify(() => mockCallback.onCallback("hello")).called(2);
+      });
+    },
+  );
 }
